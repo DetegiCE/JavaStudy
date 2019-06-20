d0734c1a08acd32af239229a3205e6ee3f38e7ea
DetegiCE <martinok1103@naver.com>
2019-06-20 23:51:56 +0900

    Create CircleArea.java

---
+++ chapter2/CircleArea.java
@@ -0,0 +1,10 @@
+
+public class CircleArea {
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		double radius = 4.5;
+		double circleArea = radius*radius*PI;
+		
+		System.out.println("면적 = "+circleArea);
+	}
+}
