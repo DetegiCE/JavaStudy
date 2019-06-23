75701c60753dec1bf403ab53096290f1495976da
DetegiCE <martinok1103@naver.com>
2019-06-23 12:00:37 +0900

    Create GarbageEx.java

---
+++ chapter4/GarbageEx.java
@@ -0,0 +1,11 @@
+public class GarbageEx {
+	public static void main(String[] args) {
+		String a = new String("Java");
+		String b = new String("C++");
+		String c = new String("Python");
+		String d, e;
+		a = null;
+		d = c;
+		c = null;
+	}
+}
