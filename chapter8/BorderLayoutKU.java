3d49f202f44ee0530a4caef1027784bdd68352c0
DetegiCE <martinok1103@naver.com>
2019-06-25 21:38:21 +0900

    Create BorderLayoutKU.java

---
+++ chapter8/BorderLayoutKU.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class BorderLayoutKU extends JFrame {
+	BorderLayoutKU() {
+		super("BorderLayout이다!");
+		
+		Container cp = getContentPane();
+		
+		cp.setLayout(new BorderLayout(30, 20));
+		cp.add(new JButton("SKMirae"), BorderLayout.EAST);
+		cp.add(new JButton("Gyoyang"), BorderLayout.WEST);
+		cp.add(new JButton("Hakgwan"), BorderLayout.SOUTH);
+		cp.add(new JButton("Gukje"), BorderLayout.NORTH);
+		cp.add(new JButton("Minju"), BorderLayout.CENTER);
+		
+		setSize(350,250);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		BorderLayoutKU blk = new BorderLayoutKU();
+	}
+}
