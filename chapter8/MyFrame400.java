9343576a517ceb2713e1b21d9f925f1d5cdfb6c4
DetegiCE <martinok1103@naver.com>
2019-06-25 20:43:11 +0900

    Create MyFrame400.java

---
+++ chapter8/MyFrame400.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MyFrame400 extends JFrame {
+	MyFrame400() {
+		setTitle("MyFrame400x400");
+		setSize(400,400);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		MyFrame400 mf4 = new MyFrame400();
+	}
+}
